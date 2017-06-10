f70f174824cdb77a8faf13453e80cdf786ae4338
Douglas Duteil <douglasduteil@gmail.com>
2017-06-10 17:32:31 +0200

    SAVE

--- src/app/game/monitor/monitor.component.ts
+++ src/app/game/monitor/monitor.component.ts
@@ -1,18 +1,43 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, HostListener, Input, OnInit, ViewChild } from "@angular/core";
+
+const KEY_CODE = {
+  A: 65,
+};
+
+const STARTING_VIDEO = [
+  "assets/videos/skate_01.webm",
+  "assets/videos/skate_02.webm",
+];
+const END_VIDEO = [
+  "assets/videos/skate_03.webm",
+];
 
 @Component({
   selector: "paf-monitor",
   styleUrls: ["./monitor.component.scss"],
   templateUrl: "./monitor.component.html",
 })
-export class MonitorComponent implements OnInit {
+export class MonitorComponent {
+  @ViewChild("videoRef")
+  public videoRef: ElementRef;
+  @Input()
+  public letter: string;
+  public startingVideoId = 0;
+  public endingVideoId = 0;
+  public src = STARTING_VIDEO[this.startingVideoId];
 
-  constructor() {
-
+  @HostListener("window:keyup", ["$event"])
+  public keyEvent(event: KeyboardEvent) {
+    if (event.key === this.letter) {
+      this.src = END_VIDEO[this.endingVideoId];
+      this.endingVideoId = (this.endingVideoId + 1) % END_VIDEO.length;
+      this.videoRef.nativeElement.load();
+    }
   }
 
-  public ngOnInit() {
-
+  public onVideoEnd(event: HTMLVideoElementEventMap) {
+    this.src = STARTING_VIDEO[this.startingVideoId];
+    this.startingVideoId = (this.startingVideoId + 1) % STARTING_VIDEO.length;
+    this.videoRef.nativeElement.load();
   }
-
 }
