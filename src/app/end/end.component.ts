ddc44bec563afb3edd67c399cd557946c360176b
Douglas Duteil <douglasduteil@gmail.com>
2017-06-11 13:44:56 +0200

    fix some bugs

--- src/app/end/end.component.ts
+++ src/app/end/end.component.ts
@@ -1,8 +1,22 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, HostListener, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "paf-end",
   styleUrls: ["./end.component.scss"],
   templateUrl: "./end.component.html",
 })
-export class EndComponent {}
+export class EndComponent {
+  constructor(
+    private router: Router,
+  ) {}
+  @HostListener("window:keyup", ["$event"])
+  public keyEvent(event: KeyboardEvent) {
+    if (event.key === " ") {
+      this.router.navigate(["/game"]);
+    }
+    if (event.key === "ArrowUp") {
+      this.router.navigate(["/start-menu"]);
+    }
+  }
+}
